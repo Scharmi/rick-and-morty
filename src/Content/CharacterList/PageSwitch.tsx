57d38d4d54d7e7aef129dd0f23d3519e7ff1dc51
Scharmi <scharmi1500@gmail.com>
2022-05-17 23:27:30 +0200

    Added underline on hover in PageSwitch

--- src/Content/CharacterList/PageSwitch.tsx
+++ src/Content/CharacterList/PageSwitch.tsx
@@ -5,33 +5,47 @@ interface Props {
     maxPage: number;
     setPage: Function;
 }
-export const PageSwitchContent = styled.div`
+const PageSwitchContent = styled.div`
     text-align: center;
     font-size: 2vw;
     color: black
 `
+const PageText = styled.span`
+    cursor: pointer;
+    &:hover {
+        text-decoration: underline;
+    }
+`
+
 export function PageSwitch(props: Props) {
     let pageLinks = [];
     for(let i = Math.max(props.pageNumber-3, 1); i <= Math.min(props.pageNumber+3, Math.floor(props.maxPage)); i++) {
         if(i === props.pageNumber) {
             pageLinks.push(
-                <span 
-                    key={i}
-                    onClick={() => {props.setPage(i)}} 
-                    style={{fontWeight: "bold", cursor: "pointer"}}
-                >
-                {i + " "} 
+                <span key={i}>
+                    <PageText 
+                        key={i}
+                        onClick={() => {props.setPage(i)}} 
+                        style={
+                            {
+                            fontWeight: "bold", 
+                        }}
+                    >
+                    {i} 
+                    </PageText>
+                    {" "}
                 </span>
             )
         }
         else {
             pageLinks.push(
-                <span 
-                    key={i}
-                    onClick={() => {props.setPage(i)}} 
-                    style={{ cursor: "pointer"}}
-                >
-                {i + " "} 
+                <span key={i}>
+                    <PageText 
+                        onClick={() => {props.setPage(i)}} 
+                    >
+                    {i} 
+                    </PageText>
+                    {" "}
                 </span>
             )
         }
@@ -40,9 +54,9 @@ export function PageSwitch(props: Props) {
     if(Math.floor(props.maxPage) === 0) return <></>
     return (
         <PageSwitchContent>
-            <span onClick={() => {props.setPage(1)}} style={{cursor: "pointer"}}>&lt; </span>
+            <PageText onClick={() => {props.setPage(1)}} style={{cursor: "pointer"}}>&lt; </PageText>
             {pageLinks}
-            <span onClick={() => {props.setPage(Math.floor(props.maxPage))}} style={{cursor: "pointer"}}> &gt;</span>
+            <PageText onClick={() => {props.setPage(Math.floor(props.maxPage))}} style={{cursor: "pointer"}}> &gt;</PageText>
         </PageSwitchContent>
     )
 }
